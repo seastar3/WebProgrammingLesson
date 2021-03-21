e8e116827a86f5fbf4b4538ab78cb4a60d6a9280
seastar3 <seastar@orion.nifty.jp>
2021-03-21 19:39:19 +0900

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,47 @@
 // Node.jsのToDoリストのデータ登録担当(..\server.js \public\index.html とセットで作動する。)
+function render(todos){
+    $(".todos").children().not(".template").remove();
+    const template = $(".todos").children(".template");
+
+    todos.forEach((todo) => {
+        const node = template.clone(true).show().removeClass("template")
+
+        node.find(".text").text(todo.text);
+        node.find(".id").text(todo._id);
+        $(".todos").append(node);
+    });
+}
+function getTodos(){
+    fetch('/api/todos')
+        .then((data) => data.json())
+        .then((json) => {
+            const todos = json;
+            render(todos);
+        });
+}
+
+function createTodo(){
+    const text = $(".new-todo-text").val();
+    fetch('/api/todos', {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({text: text}),
+        })
+        .then(() => {
+            getTodos();
+        });
+}
+
+function deleteTodo(el){
+    const id = $(el).closest(".todo").find(".id").text();
+    fetch(`/api/todos/${id}`, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            getTodos();
+        });
+}
+
+$(getTodos);
