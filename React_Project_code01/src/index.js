7a2f2f90744a9a94f792e307588dda125231edd0
seastar3 <seastar@orion.nifty.jp>
2021-03-22 17:19:34 +0900

    Create index.js

---
+++ React_Project_code01/src/index.js
@@ -0,0 +1,10 @@
+import React fron 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from "./App';
+import reportWebVitals from './reportWebVitals';
+ReatDOM.render(
+  <React.StrictMode>
+  document.getElementById('root')
+);
+reportWebVitals();
